8ecb5b4d8bebba40138f4005e07edb7104341772
RafiNaufl <nrafialdhawi@gmail.com>
2023-12-18 14:35:20 +0700

    feat: update chat room 1

--- perion-app/src/components/chat/ChatRoom.tsx
+++ perion-app/src/components/chat/ChatRoom.tsx
@@ -165,62 +165,12 @@ export function ChatRoom() {
                       </div>
                     </div>
                   </div>
-                  <div className="message mb-4 flex">
-                    <div className="flex-2">
-                      <div className="w-12 h-12 relative">
-                        <Avatar
-                          placeholder={''}
-                          src="https://docs.material-tailwind.com/img/face-2.jpg"
-                          alt="avatar"
-                        />
-                      </div>
-                    </div>
-                    <div className="flex-1 px-2">
-                      <div className="inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700">
-                        <span>
-                          All travel expenses are covered by us of course :D
-                        </span>
-                      </div>
-                      <div className="pl-4">
-                        <small className="text-gray-500">15 April</small>
-                      </div>
-                    </div>
-                  </div>
                   <div className="message me mb-4 flex text-right">
                     <div className="flex-1 px-2">
                       <div className="inline-block bg-blue-600 rounded-full p-2 px-6 text-white">
                         <span>Its like a dream come true</span>
                       </div>
                       <div className="pr-4">
-                        <small className="text-gray-500">15 April</small>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="message me mb-4 flex text-right">
-                    <div className="flex-1 px-2">
-                      <div className="inline-block bg-blue-600 rounded-full p-2 px-6 text-white">
-                        <span>I accept. Thank you very much.</span>
-                      </div>
-                      <div className="pr-4">
-                        <small className="text-gray-500">15 April</small>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="message mb-4 flex">
-                    <div className="flex-2">
-                      <div className="w-12 h-12 relative">
-                        <Avatar
-                          placeholder={''}
-                          src="https://docs.material-tailwind.com/img/face-2.jpg"
-                          alt="avatar"
-                        />
-                      </div>
-                    </div>
-                    <div className="flex-1 px-2">
-                      <div className="inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700">
-                        <span>You are welome. We will stay in touch.</span>
-                      </div>
-                      <div className="pl-4">
                         <small className="text-gray-500">15 April</small>
                       </div>
                     </div>
