0900f917342affbbb85143232f61f8b9fbe45934
Jeremiakim <jeremikimzu67@gmail.com>
2023-12-19 13:19:42 +0700

    dont changes

--- perion-app/src/components/portofolio/portoForm.tsx
+++ perion-app/src/components/portofolio/portoForm.tsx
@@ -10,7 +10,13 @@ import {
   Textarea,
 } from '@material-tailwind/react'
 
-const PortfolioForm = ({ open , handleOpen } : {open: boolean, handleOpen : () => void} ) => {
+const PortfolioForm = ({
+  open,
+  handleOpen,
+}: {
+  open: boolean
+  handleOpen: () => void
+}) => {
   return (
     <>
       <Dialog
@@ -24,31 +30,25 @@ const PortfolioForm = ({ open , handleOpen } : {open: boolean, handleOpen : () =
           unmount: { scale: 0.9, y: -100 },
         }}
       >
-      <DialogHeader
-        placeholder={''}
-        className="justify-center border-b-2 mt-2 flex flex-col lg:flex-row lg:items-center lg:justify-center gap-4"
-      >
-        <div className="flex items-center gap-3">
-          <div className="mt-px flex flex-col">
-            <p className='font-bold text-lg'>
-            Create a Portfolio
-            </p>
-            <p
-              className="mb-1 font-normal text-sm"
-            >
-              Insert your portofolio:
-            </p>
+        <DialogHeader
+          placeholder={''}
+          className="justify-center border-b-2 mt-2 flex flex-col lg:flex-row lg:items-center lg:justify-center gap-4"
+        >
+          <div className="flex items-center gap-3">
+            <div className="mt-px flex flex-col">
+              <p className="font-bold text-lg">Create a Portfolio</p>
+              <p className="mb-1 font-normal text-sm">
+                Insert your portofolio:
+              </p>
+            </div>
           </div>
-        </div>
-      </DialogHeader>
-      <DialogBody
+        </DialogHeader>
+        <DialogBody
           placeholder={''}
           className="h-auto sm:h-[30rem] md:h-[35rem] lg:h-[37rem] overflow-y-auto flex flex-col gap-3"
         >
-        <form action="">
-            <p className='font-bold text-lg text-black'>
-              Title
-            </p>
+          <form action="">
+            <p className="font-bold text-lg text-black">Title</p>
             <Input
               crossOrigin={''}
               label="Insert Title"
@@ -57,7 +57,7 @@ const PortfolioForm = ({ open , handleOpen } : {open: boolean, handleOpen : () =
               // value={input.title}
               // onChange={onChange}
             />
-            <p className='mt-3 mb-2 font-bold text-lg text-black'>
+            <p className="mt-3 mb-2 font-bold text-lg text-black">
               Description
             </p>
             <Textarea
@@ -68,8 +68,8 @@ const PortfolioForm = ({ open , handleOpen } : {open: boolean, handleOpen : () =
               // value={input.workDescription}
               // onChange={onChange}
             />
-            <p className='mt-3 mb-2 font-bold text-lg text-black'>
-            Website Link
+            <p className="mt-3 mb-2 font-bold text-lg text-black">
+              Website Link
             </p>
             <Input
               crossOrigin={''}
@@ -79,10 +79,9 @@ const PortfolioForm = ({ open , handleOpen } : {open: boolean, handleOpen : () =
               // value={input.jobLocation}
               // onChange={onChange}
             />
-            
-        </form>
-      </DialogBody>  
-      <DialogFooter
+          </form>
+        </DialogBody>
+        <DialogFooter
           placeholder={''}
           className="justify-between flex flex-col border-t-2 lg:flex-row lg:items-center lg:justify-between"
         >
@@ -91,7 +90,7 @@ const PortfolioForm = ({ open , handleOpen } : {open: boolean, handleOpen : () =
               Create Portfolio
             </Button>
           </div>
-      </DialogFooter>
+        </DialogFooter>
       </Dialog>
     </>
   )
